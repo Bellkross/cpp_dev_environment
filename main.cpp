d29f168236f0341d5dd9aafafa1fc3d3237eaa7b
Hollow <21212488+Bellkross@users.noreply.github.com>
2023-12-22 20:01:00 +0000

    Finished the watermelon

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@
 int main() {
     int watermelon_weight;
     std::cin >> watermelon_weight;
-    std::string result = watermelon_weight % 2 == 0 ? "YES" : "NO";
+    std::string result;
+    if (watermelon_weight <= 2) {
+        result = "NO";
+    } else {
+        result = watermelon_weight % 2 == 0 ? "YES" : "NO";
+    }
     std::cout << result << std::endl;
     return 0;
 }
