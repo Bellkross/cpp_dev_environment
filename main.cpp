6f9f0bd447046417a37f4c7045fc043c12e99d0a
Hollow <21212488+Bellkross@users.noreply.github.com>
2023-12-25 13:53:54 +0000

    upd

--- main.cpp
+++ main.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
-int boredom(int* arr, int length)
+using namespace std;
+
+
+// https://codeforces.com/problemset/problem/455/A
+int boredom(vector<int>& integers, unordered_map<int, int>& points)
 {
-    // https://codeforces.com/problemset/problem/455/A
-    // TODO: calculate score for each point
+    int integers_size = integers.size();
+    int max = 0;
+    for (int i = 0; i < integers_size; ++i) {
+        int removed = integers[i];
+        vector<pair<int, int>> removed_pairs;
+        if (points[removed - 1]) {
+            removed_pairs.push_back(make_pair(removed - 1, points[removed - 1]));
+        }
+        if (points[removed + 1]) {
+            removed_pairs.push_back(make_pair(removed + 1, points[removed + 1]));
+        }
+        if (--points[removed] == 0) {
+            points.erase(removed);
+            removed_pairs.push_back(make_pair(removed, 1));
+        }
+        integers.erase(integers.begin() + i);
+        int res = integers[i];
+        integers.insert(integers.begin() + i, removed);
+        if (points[removed]) {
+            ++points[removed];
+        }
+        for (const auto& rp : removed_pairs) {
+            points[rp.first] = rp.second;
+        }
+    }
     return 10;
 }
 
 int main() 
 {
     int n;
-    std::cin >> n;
+    cin >> n;
     int* a = new int[n];
+    unordered_map<int, int> points;
     for (int i = 0; i < n; ++i) {
-        std::cin >> a[i];
+        cin >> a[i];
+        ++points[a[i]];
     }
-    std::cout << boredom(a, n) << std::endl;
+    vector<int> integers(points.size());
+    for (const auto& pair : points) {
+        integers.push_back(pair.first);
+    }
+    cout << boredom(integers, points) << std::endl;
     return 0;
 }
