af0e820d0a9b0afafcc952b32a0ac24cbc79ebac
Hollow <21212488+Bellkross@users.noreply.github.com>
2023-12-25 14:26:52 +0000

    Upd

--- main.cpp
+++ main.cpp
@@ -8,10 +8,14 @@ using namespace std;
 // https://codeforces.com/problemset/problem/455/A
 int boredom(vector<int>& integers, unordered_map<int, int>& points)
 {
+    if (integers.size() == 1 && points[integers[0]] == 1) {
+        return integers[0];
+    }
     int integers_size = integers.size();
     int res = 0;
     for (int i = 0; i < integers_size; ++i) {
         int removed = integers[i];
+        int removed_points = removed * points[removed];
         vector<pair<int, int>> removed_pairs;
         if (points.count(removed - 1) > 0) {
             removed_pairs.push_back(make_pair(removed - 1, points[removed - 1]));
@@ -19,12 +23,15 @@ int boredom(vector<int>& integers, unordered_map<int, int>& points)
         if (points.count(removed + 1) > 0) {
             removed_pairs.push_back(make_pair(removed + 1, points[removed + 1]));
         }
-        if (--points[removed] == 0) {
-            points.erase(removed);
-            removed_pairs.push_back(make_pair(removed, 1));
-        }
+        removed_pairs.push_back(make_pair(removed, points[removed]));
+        points.erase(removed - 1);
+        points.erase(removed);
+        points.erase(removed + 1);
         integers.erase(integers.begin() + i);
-        res = max(res, integers[i] + boredom(integers, points));
+        int curr = removed_points + boredom(integers, points);
+        if (curr > res) {
+            res = curr;
+        }
         integers.insert(integers.begin() + i, removed);
         if (points.count(removed) > 0) {
             ++points[removed];
