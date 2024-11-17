b4ae3e33b324fa956fd71b84e1158ccdb6ade72a
Hollow <21212488+Bellkross@users.noreply.github.com>
2024-11-17 22:19:51 +0000

    upd

--- main.cpp
+++ main.cpp
@@ -5,29 +5,50 @@
 #include <vector>
 #include <algorithm>
 #include <sstream>
+#include <utility>
 
 using namespace std;
 
-// 1 - no
-// 2 - no
-// 3 - no
-// 4 - 2 2
-// 5 - no
-// 6 - 2 4
-// 10 - 8 and 2 yes
-bool can_divide_in_even_number_of_kilos(const int w)
-{
-    if (w < 4)
-    {
-        return false;
-    }
-    return w % 2 == 0;
-}
+// n and m - dimensions of the grid
+// then values of the grid. n lines m values each
+// stream was intercepted, shuffled all given integers and put them in k long line
+// Output any possible value n m that couldve been provided
 
 int main()
 {
-    int w;
-    cin >> w;
-    cout << (can_divide_in_even_number_of_kilos(w) ? "YES" : "NO") << endl;
+    stringstream ss;
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+    {
+        long long k;
+        cin >> k;
+        unordered_map<long long, long long> count;
+        // find first that in below set
+        for (long long j = 0; j < k; ++j)
+        {
+            long long curr;
+            cin >> curr;
+            ++count[curr];
+        }
+        const long long tar = k - 2;
+        if (count[1] > 0 && count[tar] > 0)
+        {
+            ss << 1 << " " << tar << endl;
+            continue;
+        }
+        for (long long q = 2; q < tar; ++q)
+        {
+            if (tar % q == 0)
+            {
+                ss << tar / q << " " << q << endl;
+                break;
+            }
+        }
+    }
+
+    cout << ss.str() << endl;
+
     return 0;
 }
