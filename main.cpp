c1d8b12992a28503a52acc355d710d83b1001f9d
Hollow <21212488+Bellkross@users.noreply.github.com>
2024-11-24 15:27:42 +0000

    Upd

--- main.cpp
+++ main.cpp
@@ -9,48 +9,9 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int shortestSubarray(vector<int>& nums, int k) {
-
-    }
-};
 
 int main()
 {
-    stringstream ss;
-    int t;
-    cin >> t;
-
-    for (int i = 0; i < t; ++i)
-    {
-        long long k;
-        cin >> k;
-        unordered_map<long long, long long> count;
-        // find first that in below set
-        for (long long j = 0; j < k; ++j)
-        {
-            long long curr;
-            cin >> curr;
-            ++count[curr];
-        }
-        const long long tar = k - 2;
-        if (count[1] > 0 && count[tar] > 0)
-        {
-            ss << 1 << " " << tar << endl;
-            continue;
-        }
-        for (long long q = 2; q < tar; ++q)
-        {
-            if (tar % q == 0)
-            {
-                ss << tar / q << " " << q << endl;
-                break;
-            }
-        }
-    }
-
-    cout << ss.str() << endl;
 
     return 0;
 }
