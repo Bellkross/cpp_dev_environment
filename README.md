02cce590a8d474a8497b74690d87220d8be3437c
Hollow <21212488+Bellkross@users.noreply.github.com>
2024-11-25 21:35:23 +0000

    upd

--- README.md
+++ README.md
@@ -16,3 +16,22 @@ Rebuild:
 ```
 docker rm devenv_container && docker rmi devenv && docker build -t devenv . && docker run -it --name devenv_container devenv
 ```
+
+Node:
+
+```
+# installs fnm (Fast Node Manager)
+curl -fsSL https://fnm.vercel.app/install | bash
+
+# activate fnm
+source ~/.bashrc
+
+# download and install Node.js
+fnm use --install-if-missing 22
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v22.11.0`
+
+# verifies the right npm version is in the environment
+npm -v # should print `10.9.0`
+```
--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
-#include <set>
 #include <vector>
+#include <iterator>
 #include <algorithm>
-#include <sstream>
-#include <utility>
+#include <cmath>
 
 using namespace std;
 
 
 int main()
 {
-
     return 0;
 }
+
+/*
+n of 18
+n of 21
+n of 25
+plank len = 60
+1: 2. 18 + 21 + 25 = 64. 60 (25, 18, 17) + 60 (39, 21)
+2: 3. at least 128 total length needed. We have 60 (21, 21, 18) + 60 (18, 42) + 60 (25, 25, 10). We need 18*2 (36) + 21*2 (42) + 25*2 (50).
+3: 4. at least 192. 60*4: 60 (21, 21, 18) + 60 (21, 21, 18) + 60 (). 18*4 72 + 21*4 84 + 25*4 100
+
+*/
